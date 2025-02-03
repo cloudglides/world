0c9c1c03fd90b9294f95721108bacee86b29d9c2
cloudglides <authqtx@gmail.com>
2025-02-04 00:58:28 +0530

    added flake.nix && flake.lock

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A simple lil flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Custom package repo (update accordingly)
+    zen-browser.url = "github:pfaj/zen-browser-flake";
+  };
+
+  outputs = { nixpkgs, home-manager, zen-browser, ... }@inputs: let
+    system = "x86_64-linux"; # Change if needed
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.default
+      ];
+      specialArgs = { inherit inputs system; }; # Pass system to configuration.nix
+    };
+  };
+}
+
